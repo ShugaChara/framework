be38843439fe4814f39c80495b4b782ca9536978
linshan <1099013371@qq.com>
2019-11-16 21:35:51 +0800

    update

---
+++ src/Swoole/RPCClient.php
@@ -0,0 +1,24 @@
+<?php
+// +----------------------------------------------------------------------
+// | Created by linshan. 版权所有 @
+// +----------------------------------------------------------------------
+// | Copyright (c) 2019 All rights reserved.
+// +----------------------------------------------------------------------
+// | Technology changes the world . Accumulation makes people grow .
+// +----------------------------------------------------------------------
+// | Author: kaka梦很美 <1099013371@qq.com>
+// +----------------------------------------------------------------------
+
+namespace ShugaChara\Framework\Swoole;
+
+use ShugaChara\Swoole\Contracts\RPCClientAbstract;
+
+/**
+ * RPC Client
+ * Class RPCClient
+ * @package ShugaChara\Framework\Swoole
+ */
+abstract class RPCClient extends RPCClientAbstract
+{
+
+}
