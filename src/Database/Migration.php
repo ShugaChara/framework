3b5863b87799e2fd38ace23fe9576d4115fab818
linshan <1099013371@qq.com>
2019-11-18 23:10:48 +0800

    uppdate

---
+++ src/Database/Migration.php
@@ -0,0 +1,23 @@
+<?php
+// +----------------------------------------------------------------------
+// | Created by linshan. 版权所有 @
+// +----------------------------------------------------------------------
+// | Copyright (c) 2019 All rights reserved.
+// +----------------------------------------------------------------------
+// | Technology changes the world . Accumulation makes people grow .
+// +----------------------------------------------------------------------
+// | Author: kaka梦很美 <1099013371@qq.com>
+// +----------------------------------------------------------------------
+
+namespace ShugaChara\Framework\Database;
+
+use ShugaChara\Databases\Migration as DatabasesMigration;
+
+/**
+ * Class Migration
+ * @package ShugaChara\Framework\Database
+ */
+class Migration extends DatabasesMigration
+{
+
+}
